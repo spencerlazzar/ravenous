2399b9d72e8337ba613c49644fc949e6e6cde98f
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-08-02 23:27:38 -0700

    First Components

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Business from '../Business/Business';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      <Business />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+const BusinessList = () => {
+  return (
+    <div className='BusinessList'>
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+    </div>
+  );
+};
+
+export default BusinessList;
