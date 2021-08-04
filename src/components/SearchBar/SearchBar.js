ec26499112149271abb16f7ddc6a2971ea814431
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-08-03 19:54:05 -0700

    Search Bar Component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import './SearchBar.css';
+
+const SearchBar = () => {
+  const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count',
+  };
+
+  return <div></div>;
+};
+
+export default SearchBar;
